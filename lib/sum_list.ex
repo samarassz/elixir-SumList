99281bacc78109f38a1d5c7ee6d77c3b424c4594
samara <65817917+samarassz@users.noreply.github.com>
2022-01-02 18:20:27 -0300

    usando Enum.sum()

--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -1,6 +1,9 @@
 # Soma todos os elementos de uma lista passada pelo usuário
 
 defmodule SumList do
+  # usando Enum.sum
+  def call_enum(list), do: Enum.sum(list)
+
   # definindo uma função principal
   def call(list), do: sum(list, 0)
 
